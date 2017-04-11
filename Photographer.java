288f10caeb771833b2010e9ba4a84aaffa504105
Bill Proudfoot <user@CODECLAN031.local>
2017-04-11 15:26:56 +0100

    Photographer Homework files added

---
+++ Photographer.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Photographer{
+
+  private String name;
+
+  private ArrayList<Collection> bag;
+
+  public Photographer(String name){
+    this.name = name;
+    this.bag = new ArrayList<Collection>();
+
+    public String getName(){
+      return this.name;
+    }
+    
+  }
+}
